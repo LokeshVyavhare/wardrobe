7fe1998c7b32e5b1a03df0ae012d293dd3615bcf
Shivang kumar <103144321+kshivang80@users.noreply.github.com>
2022-11-10 04:10:21 +0530

    Shivang Landing page

--- Front-end/src/Redux/store.js
+++ Front-end/src/Redux/store.js
@@ -1,13 +1,9 @@
 import {authReducer} from './Auth/authReducer'
-import {cartReducer} from './Cart/cartReducer'
-import {productReducer} from './Product/productReducer'
 import { legacy_createStore, compose, applyMiddleware, combineReducers } from 'redux';
 import thunk from 'redux-thunk';
 
 const rootReducer = combineReducers({
-    auth: authReducer,
-    cart: cartReducer,
-    product : productReducer
+    auth: authReducer
 })
 
 const createComposer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
