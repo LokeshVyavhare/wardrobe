2be0b1b4ae1bf36eaaef1bdf394b6f5258c1b388
abhizeit <abhijeitsarraf@gmail.com>
2022-11-09 21:40:24 +0530

    cart-route

--- Back-end/src/features/products/product.routes.js
+++ Back-end/src/features/products/product.routes.js
@@ -1,62 +1,62 @@
 const express = require("express");
-const Product = require("./product.model")
+const Product = require("./product.model");
 
 const app = express.Router();
 
-app.get("/" , async (req,res) => {
-    const {limit=5 , page=1} = req.query;
-    try {
-        const product = await Product.find().limit(limit).skip((page-1) * limit);
-        res.send(product);
-    } catch (error) {
-        res.status(404).send(error.message)
-    }
+app.get("/", async (req, res) => {
+  const { limit = 5, page = 1 } = req.query;
+  try {
+    const product = await Product.find()
+      .limit(limit)
+      .skip((page - 1) * limit);
+    res.send(product);
+  } catch (error) {
+    res.status(404).send(error.message);
+  }
 });
 
-app.get("/:id" , async (req,res) => {
-    const {limit=10, page=1} = req.query;
-    try {
-        let id = req.params.id;
-        let product = await Product.findById(id).limit(limit).skip((page-1) * limit);
-        res.send(product);  
-    } catch (error) {
-        res.send(404).send(error.message);
-    }
-   
+app.get("/:id", async (req, res) => {
+  const { limit = 10, page = 1 } = req.query;
+  try {
+    let id = req.params.id;
+    let product = await Product.findById(id)
+      .limit(limit)
+      .skip((page - 1) * limit);
+    res.send(product);
+  } catch (error) {
+    res.send(404).send(error.message);
+  }
 });
 
-app.get("/category" , async (req,res) => {
-    try {
-    let {category} = req.params;
+app.get("/category", async (req, res) => {
+  try {
+    let { category } = req.params;
     let product = await Product.find(category);
-    res.send(product)
-    } catch (error) {
-    res.status(404).send(error.message);   
-    }
-    
-})
+    res.send(product);
+  } catch (error) {
+    res.status(404).send(error.message);
+  }
+});
 
-app.get("/tags" , async (req,res) => {
-    try {
-    let {tags} = req.query;
+app.get("/tags", async (req, res) => {
+  try {
+    let { tags } = req.query;
     let product = await Product.find(tags);
-    res.send(product)
-    } catch (error) {
-    res.status(404).send(error.message);   
-    }
-    
-})
+    res.send(product);
+  } catch (error) {
+    res.status(404).send(error.message);
+  }
+});
 
-app.post("/" , async (req,res) => {
-    try {
-        let product = await Product.create({
-            ...req.body
-        });
-        res.send(product)
-    } catch (error) {
-        res.status(404).send(error.message);
-    }
-})
+app.post("/", async (req, res) => {
+  try {
+    let product = await Product.create({
+      ...req.body,
+    });
+    res.send(product);
+  } catch (error) {
+    res.status(404).send(error.message);
+  }
+});
 
-module.exports = app
-
+module.exports = app;
--- Back-end/src/index.js
+++ Back-end/src/index.js
@@ -2,18 +2,17 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const connect = require("./config/db");
-const userRouter = require("./features/user/user.routes");
+const userRoutes = require("./features/user/user.routes");
 const productRoutes = require("./features/products/product.routes");
 const cartRoutes = require("./features/cart/cart.routes");
 const PORT = process.env.port || 8080;
 const app = express();
 app.use(express.json());
 app.use(cors());
-app.use("/user", userRouter);
 
-app.use("/users" , userRouter)
-app.use("/products" , productRoutes);
-app.use("/carts" , cartRoutes)
+app.use("/users", userRoutes);
+app.use("/products", productRoutes);
+app.use("/carts", cartRoutes);
 
 app.listen(PORT, async () => {
   await connect();
