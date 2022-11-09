0759154e59976b7a49a5e57a761a7a65a989eeb3
somya ranjan singh <maaheersj@gmail.com>
2022-11-09 15:41:18 +0530

    product added

--- Back-end/src/index.js
+++ Back-end/src/index.js
@@ -3,10 +3,13 @@ const express = require("express");
 const cors = require("cors");
 const connect = require("./config/db");
 const userRouter = require("./features/user/user.routes");
+const productRoutes = require("./features/products/product.routes")
 const PORT = process.env.port || 8080;
 const app = express();
 app.use(express.json());
 app.use(cors());
+
+app.use("/products" , productRoutes)
 
 app.listen(PORT, async () => {
   await connect();
