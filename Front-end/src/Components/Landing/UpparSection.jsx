d830dcfa62c57c76fb9a1dff3a6e41e1f0ffd6eb
Shivang kumar <103144321+kshivang80@users.noreply.github.com>
2022-11-10 23:06:03 +0530

    landing and footer

--- Front-end/src/Components/Landing/UpparSection.jsx
+++ Front-end/src/Components/Landing/UpparSection.jsx
@@ -5,11 +5,11 @@ import { Login } from '../Registration/Login'
 
 const UpparSection = () => {
     return (
-        <div>
-            <div className="Mbox1" style={{ border: "1px solid gray", height: "40px", width: "80%" ,display:"flex",justifyContent:"space-evenly",overflow:"hidden",position:"fixed",top:"0", marginLeft: "10%",boxShadow: "rgba(99, 99, 99, 0.2) 0px 2px 8px 0px",backgroundColor:"white"}}>
+        <div >
+            <div className="Mbox1" style={{ border: "1px solid gray",borderTop:"none",borderRight:"none" ,borderLeft:"none" ,height: "40px", width: "80%", display: "flex", justifyContent: "space-evenly", overflow: "hidden", position: "fixed", top: "0.1px", marginLeft: "10%",  backgroundColor: "white" }}>
 
-                <div className='Sbox11' style={{  height: "auto", width: "40%", margin: "auto" }}>
-                   <Select className='country1' placeholder='Country' size='sm' width="120px"  border="none" color="black" >
+                <div className='Sbox11' style={{ height: "auto", width: "40%", margin: "auto" }}>
+                    <Select className='country1' placeholder='Country' size='sm' width="120px" border="none" color="black" >
                         <option value='option1'>INDIA</option>
                         <option value='option2'>USA</option>
                         <option value='option3'>RUSSIA</option>
@@ -22,32 +22,33 @@ const UpparSection = () => {
                         <option value='option1'>India</option>
                         <option value='option2'>Usa</option>
                         <option value='option3'>China</option>
-                   </Select>
+                    </Select>
                     <div className='customer' style={{ marginTop: "-29px", marginLeft: "-110px" }}>
                         {/* <h5 style={{ color:"black"}}>CUSTOMER CARE</h5> */}
-                        <p style={{fontSize:"13px",fontWeight:"bold",marginTop:"2px"}}>CUSTOMER CARE</p>
+                        <p style={{ fontSize: "13px", fontWeight: "bold", marginTop: "2px" }}>CUSTOMER CARE</p>
                     </div>
 
 
                 </div>
 
 
-                <div className='Sbox11' style={{  height: "auto", width: "40%", margin: "auto",display:"flex",justifyContent:"flex-end" }}>
-                    <Register/>
-                    <Login/>
+                <div className='Sbox11' style={{ height: "auto", width: "40%", margin: "auto", display: "flex", justifyContent: "flex-end" }}>
+                    <Register />
+                    <Login />
 
                 </div>
-                
-
-              
 
 
+
+
+               
 
             </div>
-          <div className='line'>
-            <hr style={{ borderTop: "1px solid gray"}} />
+            {/* <div className='line'>
+                    <hr style={{ borderTop: "1px solid red" ,marginTop:"10px"}} />
 
-          </div>
+                </div> */}
+
 
         </div>
     )
