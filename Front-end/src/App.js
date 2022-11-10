a75d5a808c5806c0f1265da0d65062d54b9a106b
LV-23 <lokeshvyavhare23@gmail.com>
2022-11-10 10:25:59 +0530

    Solving The issue

--- Front-end/src/App.js
+++ Front-end/src/App.js
@@ -1,16 +1,14 @@
 import './App.css';
 import { Footer } from './Components/Footer/Footer';
-import { Landing } from './Components/Landing/Landing';
 import { Navbar } from './Components/Navbar/Navbar';
 import {Router} from './Components/Router/Router'
 
 function App() {
   return (
     <div className="App">
-      <Landing/>
-      {/* <Navbar />
+      <Navbar />
       <Router />
-      <Footer /> */}
+      <Footer />
     </div>
   );
 }
--- Front-end/src/Components/Cart/cart.jsx
+++ Front-end/src/Components/Cart/cart.jsx
@@ -1,7 +1,88 @@
-import { Heading, Text } from "@chakra-ui/react"
+import {
+    Heading,
+    Text,
+    Flex,
+    Box,
+    Link,
+    Image,
+    RadioGroup,
+    Radio,
+    Stack
+
+} from "@chakra-ui/react"
+import style from './cart.module.css'
+import data from '../../db.json';
+import { useState } from "react";
+import { useEffect } from "react";
+import { CartItem } from "./CartItem";
+import { RiArrowRightSLine } from 'react-icons/ri'
+
+const cart = data.cart;
 
 export const Cart = () => {
-    return <div>
-            <Text class>Shopping Bag</Text>
+    const [data, setData] = useState([]);
+    useEffect(() => {
+        setData(cart);
+    }, [])
+    console.log(cart)
+    return <div className={style.cart}>
+        <Text className={style.heading}>Shopping Bag</Text>
+
+
+        <Flex className={style.payment}>
+            <Box className={style.link}><Link>BACK TO SHOPPING</Link></Box>
+
+            <Flex className={style.paymentOpt}>
+                <Flex className={style.paypalBox}>
+                    <Image src='https://www.yoox.com/media/yoox16/logos/paypal_logo_txt.png?v=1'></Image>
+                    <Text>CHECK OUT</Text>
+                </Flex>
+
+                <Flex className={style.proceedToOrder}>
+                    <Text>PROCEED WITH ORDERS</Text>
+                    <RiArrowRightSLine className={style.icon} />
+                </Flex>
+
+            </Flex>
+        </Flex>
+
+        <Box className={style.cartList} borderBottom={['6px solid rgb(228, 228, 228)']}>
+            <Heading size={'md'} p={['10px 12px']}>
+                <Flex>
+                    <Text mr={['25px']}>1</Text>
+                    <Text mr={['25px']}>|</Text>
+                    <Text mr={['25px']}>ITEMS ADDED TO YOUR SHOPPING BAG {1}</Text>
+                </Flex>
+            </Heading>
+            {data.map((item) => <CartItem key={item.id + 'cartData'} data={item} />)}
+        </Box>
+        <Box className={style.cartList}>
+            <Heading size={'md'} p={['10px 12px']}>
+                <Flex>
+                    <Text mr={['25px']}>2</Text>
+                    <Text mr={['25px']}>|</Text>
+                    <Text mr={['25px']}> SELECT SHIPPING METHOD {1}</Text>
+                </Flex>
+            </Heading>
+
+            <Box >
+                <Flex>
+                    <input type="radio" name="mode"/>
+                    <Box>
+                    <Heading size={'sm'}>STANDARD - $ 9.95</Heading>
+                    <Text color='grey' fontSize={'13px'}>8-10 business days</Text>
+                    </Box>
+                </Flex>
+                <Flex>
+                    <input type="radio" name="mode"/>
+                    <Box>
+                    <Heading size={'sm'}>EXPRESS -$ 14.95</Heading>
+                    <Text color='grey' fontSize={'13px'}>2-3 business days</Text>
+                    </Box>
+                    
+                </Flex>
+            </Box>
+
+        </Box>
     </div>
 }
