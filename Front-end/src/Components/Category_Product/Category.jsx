f89d2f62191d065b5792633019dc4737f4a2acfa
somya ranjan singh <maaheersj@gmail.com>
2022-11-10 19:00:13 +0530

    changes

--- Front-end/src/Components/Category_Product/Category.jsx
+++ Front-end/src/Components/Category_Product/Category.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import {useSelector , useDispatch} from "react-redux"
 import { getCategory } from '../../Redux/Category/actions';
+import styled from "styled-components";
 
 const Category = () => {
 const {data, error , loading }= useSelector((store) => store.category);
@@ -11,10 +12,46 @@ React.useEffect(() => {
 }, [])
 
   return (
-    <div className='category'>
-      
+    <div>
+      <Wrapper>
+
+        <WrapperFilter>
+
+        </WrapperFilter>
+
+        <WrapperProducts>
+         {
+            data.map((el) => (
+                <div key={el.id}>
+                 
+                </div>
+            ))
+         }
+        </WrapperProducts>
+
+      </Wrapper>
     </div>
   )
 }
+
+const Wrapper = styled.div`
+border : 1px solid red;
+display : flex;
+height : auto;
+`;
+
+const WrapperFilter = styled.div`
+width : 200px;
+border : 1px solid black;
+`;
+
+const WrapperProducts = styled.div`
+width : 100%;
+border: 1px solid blue;
+display : grid;
+grid-template-columns : repeat(auto-fit, minmax(200px,max-content));
+justify-content : center;
+grid-gap : 10px
+`;
 
 export  {Category}
