bc880ccfd641686bb0ccd5c3675c4f87af327ca0
suraj sanjay kokane <108039887+kokanesuraj007@users.noreply.github.com>
2022-11-11 14:08:07 +0530

    fw20_0195

---
+++ Front-end/src/Components/Womens/Slider.jsx
@@ -0,0 +1,85 @@
+import React, { useState } from 'react';
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+import { data} from './data';
+import { Heading } from '@chakra-ui/react';
+function Sliderr() {
+  const [defaultImage, setDefaultImage] = useState({});
+  const settings = {
+    dots: true,
+    infinite: false,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    initialSlide: 0,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          initialSlide: 2,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  };
+
+  const handleErrorImage = (data) => {
+    setDefaultImage((prev) => ({
+      ...prev,
+      [data.target.alt]: data.target.alt,
+      
+    }));
+  };
+
+  return (
+    <div className="Apppp">
+      <Slider {...settings}>
+        {data.map((item) => (
+          <div className="card">
+            <div className="card-top">
+              <img
+                src={
+                  defaultImage[item.title] === item.title
+                    ? defaultImage.linkDefault
+                    : item.link
+                }
+                alt={item.title}
+                onError={handleErrorImage}
+              />
+              <Heading size="sm">{item.title}</Heading>
+            </div>
+            <div className="card-bottom">
+            <span className="category">{item.category}</span>
+            <h3>{item.offer}</h3>
+              <Heading size="sm">{item.price}</Heading>
+              
+            </div>
+          </div>
+        ))}
+      </Slider>
+    </div>
+  );
+}
+
+export default Sliderr;
+
+
+
---
+++ Front-end/src/Components/Womens/data.jsx
@@ -0,0 +1,455 @@
+export const data=[
+    {
+        id:1,
+        title:"MAJE",
+        category:"Cardigan",
+        offer:"41% OFF",
+        price:"$220.00",
+        link:"https://www.yoox.com/images/items/14/14270715XR_14_f.jpg?impolicy=crop&width=306&height=390"
+    },
+    {
+        id:2,
+        title:"MM6 MAISON MARGIELA",
+        category:"Ring",
+        offer:"26% OFF",
+        price:"$144.00",
+        link:"https://www.yoox.com/images/items/50/50291958CE_14_f.jpg?impolicy=crop&width=306&height=390"
+    },
+    {
+        id:3,
+        title:"MM6 MAISON MARGIELA",
+        category:"Bracelete",
+        offer:"26% OFF",
+        price:"$144.00",
+        link:"https://www.yoox.com/images/items/50/50291961LR_14_f.jpg?impolicy=crop&width=306&height=390"
+    },
+    {
+        id:4,
+        title:"RE/DONE with LEVI'S",
+        category:"Denim pants",
+        offer:"41% OFF",
+        price:"$296.00",
+        link:"https://www.yoox.com/images/items/13/13908422SI_14_f.jpg?impolicy=crop&width=306&height=390"
+    },
+    {
+        id:5,
+        title:"Hanro",
+        category:"SLeepwear",
+        offer:"26% OFF",
+        price:"$165.00",
+        link:"https://www.yoox.com/images/items/48/48291010FA_14_f.jpg?impolicy=crop&width=306&height=390"
+    },
+    {
+        id:6,
+        title:"Vivinne westwood",
+        category:"Earrings",
+        offer:"41% OFF",
+        price:"$124.00",
+        link:"https://www.yoox.com/images/items/50/50290936UM_14_f.jpg?impolicy=crop&width=306&height=390"
+    },
+    {
+        id:7,
+        title:"JOSEPH",
+        category:"Casual pants",
+        offer:"41% OFF",
+        price:"$224.00",
+        link:"https://www.yoox.com/images/items/13/13912289PA_14_f.jpg?impolicy=crop&width=306&height=390"
+    },
+    {
+        id:8,
+        title:"Bonne Maison",
+        category:"Socks & tight",
+        offer:"",
+        price:"$24.00",
+        link:"https://www.yoox.com/images/items/48/48292048TA_14_f.jpg?impolicy=crop&width=306&height=390"
+    },
+    {
+        id:9,
+        title:"MM6 MAISON MARGIELA",
+        category:"Blazer",
+        offer:"41% OFF",
+        price:"$552.00",
+        link:"https://www.yoox.com/images/items/49/49806254FP_14_f.jpg?impolicy=crop&width=306&height=390"
+    },
+    
+]
+
+export const kdata=[
+    {
+        id:1,
+        title:"CHLOE",
+        category:"Pendent",
+        offer:"32% OFF",
+        price:"$42.00",
+        link:"https://www.yoox.com/images/items/50/50288703MS_14_f.jpg?impolicy=crop&width=306&height=390"
+    },
+    {
+        id:2,
+        title:"MARNI",
+        category:"Coat",
+        offer:"56% OFF",
+        price:"$987.00",
+        link:"https://www.yoox.com/images/items/16/16157930OH_14_f.jpg?impolicy=crop&width=306&height=390"
+    },
+    {
+        id:3,
+        title:"NANUSHKA",
+        category:"Handbag",
+        offer:"39% OFF",
+        price:"$140.00",
+        link:"https://www.yoox.com/images/items/45/45698461RJ_14_f.jpg?impolicy=crop&width=306&height=390"
+    },
+    {
+        id:4,
+        title:"GIANVITO ROSSI",
+        category:"Ankle boot",
+        offer:"35% OFF",
+        price:"$920.00",
+        link:"https://www.yoox.com/images/items/17/17366392GE_14_f.jpg?impolicy=crop&width=306&height=390"
+    },
+    {
+        id:5,
+        title:"ISABLE MARANT ETOILE",
+        category:"Coat",
+        offer:"53% OFF",
+        price:"$310.00",
+        link:"https://www.yoox.com/images/items/16/16169848HN_14_f.jpg?impolicy=crop&width=306&height=390"
+    },
+    {
+        id:6,
+        title:"CHLOE",
+        category:"Boots",
+        offer:"42% OFF",
+        price:"$817.00",
+        link:"https://www.yoox.com/images/items/17/17358702KX_14_f.jpg?impolicy=crop&width=306&height=390"
+    },
+    {
+        id:7,
+        title:"LISA MARIE FERNANDEZ",
+        category:"Blazer",
+        offer:"46% OFF",
+        price:"$172.00",
+        link:"https://www.yoox.com/images/items/49/49764634UT_14_f.jpg?impolicy=crop&width=306&height=390"
+    },
+    {
+        id:8,
+        title:"ROCHAS",
+        category:"Hat",
+        offer:"42% OFF",
+        price:"$206.00",
+        link:"https://www.yoox.com/images/items/46/46864723JN_14_f.jpg?impolicy=crop&width=306&height=390"
+    },
+    {
+        id:9,
+        title:"VALENTINO",
+        category:"Mini Skirt",
+        offer:"64% OFF",
+        price:"$673.00",
+        link:"https://www.yoox.com/images/items/13/13859882NF_14_f.jpg?impolicy=crop&width=306&height=390"
+    },
+    
+]
+
+export const mkdata=[
+    {
+        id:1,
+        title:"McQ Alexander McQueen",
+        category:"Patterned shirt",
+        offer:"35% OFF",
+        price:"$223.00",
+        link:"https://www.yoox.com/images/items/12/12956627EA_14_f.jpg?impolicy=crop&width=306&height=390"
+    },
+    {
+        id:2,
+        title:" Alexander McQueen",
+        category:"Cross-body bags",
+        offer:"30% OFF",
+        price:"$655.00",
+        link:"https://www.yoox.com/images/items/45/45703381NK_14_f.jpg?impolicy=crop&width=306&height=390"
+    },
+    {
+        id:3,
+        title:"BALENCIAGA",
+        category:"Coat",
+        offer:"27% OFF",
+        price:"$2258.00",
+        link:"https://www.yoox.com/images/items/16/16174090TU_14_f.jpg?impolicy=crop&width=306&height=390"
+    },
+    {
+        id:4,
+        title:"VEJA",
+        category:"Sneakers",
+        offer:"31% OFF",
+        price:"$102.00",
+        link:"https://www.yoox.com/images/items/17/17422364UJ_14_f.jpg?impolicy=crop&width=306&height=390"
+    },
+    {
+        id:5,
+        title:"MARCELO BURLON",
+        category:"Denim shirt",
+        offer:"44% OFF",
+        price:"$467.00",
+        link:"https://www.yoox.com/images/items/12/12961067HX_14_f.jpg?impolicy=crop&width=306&height=390"
+    },
+    {
+        id:6,
+        title:"KENZO",
+        category:"Hat",
+        offer:"37% OFF",
+        price:"$170.00",
+        link:"https://www.yoox.com/images/items/46/46874986AG_14_f.jpg?impolicy=crop&width=306&height=390"
+    },
+    {
+        id:7,
+        title:"ONLY & SONS",
+        category:"Bomber",
+        offer:"",
+        price:"$120.00",
+        link:"https://www.yoox.com/images/items/16/16183151SR_14_f.jpg?impolicy=crop&width=306&height=390"
+    },
+    {
+        id:8,
+        title:"KENZO",
+        category:"Solid color shirt",
+        offer:"34% OFF",
+        price:"$207.00",
+        link:"https://www.yoox.com/images/items/12/12956680QS_14_f.jpg?impolicy=crop&width=306&height=390"
+    },
+    {
+        id:9,
+        title:"VALENTINO GARAVANI",
+        category:"Backpack & fanny pack",
+        offer:"49% OFF",
+        price:"$1072.00",
+        link:"https://www.yoox.com/images/items/45/45706179CK_14_f.jpg?impolicy=crop&width=306&height=390"
+    },
+    
+]
+
+
+export const mmkdata=[
+    {
+        id:1,
+        title:"COMME des GARCONS SHIRT",
+        category:"Patterned shirt",
+        offer:"53% OFF",
+        price:"$322.00",
+        link:"https://www.yoox.com/images/items/12/12914385LM_14_f.jpg?impolicy=crop&width=306&height=390"
+    },
+    {
+        id:2,
+        title:"VALENTINO",
+        category:"Hooded sweatshirt",
+        offer:"60% OFF",
+        price:"$2270.00",
+        link:"https://www.yoox.com/images/items/12/12921978SA_14_f.jpg?impolicy=crop&width=306&height=390"
+    },
+    {
+        id:3,
+        title:"MSGM",
+        category:"Denim pants",
+        offer:"64% OFF",
+        price:"$115.00",
+        link:"https://www.yoox.com/images/items/13/13784264LX_14_f.jpg?impolicy=crop&width=306&height=390"
+    },
+    {
+        id:4,
+        title:"JW ANDERSON",
+        category:"Patterned shirt",
+        offer:"45% OFF",
+        price:"$388.00",
+        link:"https://www.yoox.com/images/items/12/12907663TU_14_f.jpg?impolicy=crop&width=306&height=390"
+    },
+    {
+        id:5,
+        title:"KENZO",
+        category:"Striped shirt",
+        offer:"54% OFF",
+        price:"$184.00",
+        link:"https://www.yoox.com/images/items/12/12918534RP_14_f.jpg?impolicy=crop&width=306&height=390"
+    },
+    {
+        id:6,
+        title:"MARNI",
+        category:"Hooded sweatshirt",
+        offer:"53% OFF",
+        price:"$333.00",
+        link:"https://www.yoox.com/images/items/12/12904232HA_14_f.jpg?impolicy=crop&width=306&height=390"
+    },
+    {
+        id:7,
+        title:"VALENTINO GARVANI",
+        category:"Backpack & fanny pack",
+        offer:"62% OFF",
+        price:"$715.00",
+        link:"https://www.yoox.com/images/items/45/45658936UU_14_f.jpg?impolicy=crop&width=306&height=390"
+    },
+    {
+        id:8,
+        title:"ALEXANDER MCQUEEN",
+        category:"Cufflinks and Tie Clips",
+        offer:"49% OFF",
+        price:"$219.00",
+        link:"https://www.yoox.com/images/items/50/50282100XI_14_f.jpg?impolicy=crop&width=306&height=390"
+    },
+    {
+        id:9,
+        title:"VERSACE JEANS COUTURE",
+        category:"Denim Pants",
+        offer:"58% OFF",
+        price:"$159.00",
+        link:"https://www.yoox.com/images/items/13/13869103VG_14_f.jpg?impolicy=crop&width=306&height=390"
+    },
+]
+
+export const kidData1=[
+    {
+        id:1,
+        title:"DOLCE & GABBANA",
+        category:"Dress",
+        offer:"45% OFF",
+        price:"$943.00",
+        link:"https://www.yoox.com/images/items/15/15248360WV_14_f.jpg?impolicy=crop&width=306&height=390"
+    },
+    {
+        id:2,
+        title:"CHLOE",
+        category:"Ankle boot",
+        offer:"37% OFF",
+        price:"$434.00",
+        link:"https://www.yoox.com/images/items/17/17431923QO_14_f.jpg?impolicy=crop&width=306&height=390"
+    },
+    {
+        id:3,
+        title:"MIMISOL",
+        category:"Dress",
+        offer:"37% OFF",
+        price:"$198.00",
+        link:"https://www.yoox.com/images/items/15/15256971JR_14_f.jpg?impolicy=crop&width=306&height=390"
+    },
+    {
+        id:4,
+        title:"DOLCE & GABBANA",
+        category:"Boots",
+        offer:"45% OFF",
+        price:"$324.00",
+        link:"https://www.yoox.com/images/items/17/17399539VM_14_f.jpg?impolicy=crop&width=306&height=390"
+    },
+    {
+        id:5,
+        title:"RALPH LAUREN",
+        category:"Hat",
+        offer:"35% OFF",
+        price:"$51.00",
+        link:"https://www.yoox.com/images/items/46/46838521SJ_14_f.jpg?impolicy=crop&width=306&height=390"
+    },
+    {
+        id:6,
+        title:"SOPHIA WEBSTER",
+        category:"Ankle boot",
+        offer:"30% OFF",
+        price:"$198.00",
+        link:"https://www.yoox.com/images/items/17/17419707GW_14_f.jpg?impolicy=crop&width=306&height=390"
+    },
+    {
+        id:7,
+        title:"STELLA McCARTNEY KIDS",
+        category:"Sweatshirt",
+        offer:"42% OFF",
+        price:"$86.00",
+        link:"https://www.yoox.com/images/items/12/12973336IV_14_f.jpg?impolicy=crop&width=306&height=390"
+    },
+    {
+        id:8,
+        title:"RALPH LAUREN",
+        category:"Denim pants",
+        offer:"36% OFF",
+        price:"$172.00",
+        link:"https://www.yoox.com/images/items/13/13915164IC_14_f.jpg?impolicy=crop&width=306&height=390"
+    },
+    {
+        id:9,
+        title:"DOLCE GABBANA",
+        category:"Ankle boot",
+        offer:"45% OFF",
+        price:"$310.00",
+        link:"https://www.yoox.com/images/items/17/17398033VL_14_f.jpg?impolicy=crop&width=306&height=390"
+    },
+
+]
+
+export const kidData2=[
+    {
+        id:1,
+        title:"RALPH LAUREN",
+        category:"Sweatershirt",
+        offer:"55% OFF",
+        price:"$54.00",
+        link:"https://www.yoox.com/images/items/12/12789893QP_14_f.jpg?impolicy=crop&width=306&height=390"
+    },
+    {
+        id:2,
+        title:"RALPH LAUREN",
+        category:"Sweater",
+        offer:"56% OFF",
+        price:"$68.00",
+        link:"https://www.yoox.com/images/items/14/14289882GS_14_f.jpg?impolicy=crop&width=306&height=390"
+    },
+    {
+        id:3,
+        title:"RALPH LAUREN",
+        category:"Casual pants",
+        offer:"39% OFF",
+        price:"$70.00",
+        link:"https://www.yoox.com/images/items/13/13783399KU_14_f.jpg?impolicy=crop&width=306&height=390"
+    },
+    {
+        id:4,
+        title:"RALPH LAUREN",
+        category:"Denim shirt",
+        offer:"40% OFF",
+        price:"$114.00",
+        link:"https://www.yoox.com/images/items/42/42844426SO_14_f.jpg?impolicy=crop&width=306&height=390"
+    },
+    {
+        id:5,
+        title:"RALPH LAUREN",
+        category:"T-shirt",
+        offer:"55% OFF",
+        price:"$60.00",
+        link:"https://www.yoox.com/images/items/12/12949498AE_14_f.jpg?impolicy=crop&width=306&height=390"
+    },
+    {
+        id:6,
+        title:"EMPORIO ARMANI",
+        category:"Gloves",
+        offer:"35% OFF",
+        price:"$63.00",
+        link:"https://www.yoox.com/images/items/46/46854334NI_14_f.jpg?impolicy=crop&width=306&height=390"
+    },
+    {
+        id:7,
+        title:"RALPH LAUREN",
+        category:"Sweater",
+        offer:"34% OFF",
+        price:"$129.00",
+        link:"https://www.yoox.com/images/items/14/14181390FA_14_f.jpg?impolicy=crop&width=306&height=390"
+    },
+    {
+        id:8,
+        title:"NEW BALANCE",
+        category:"Sneakers",
+        offer:"",
+        price:"$77.00",
+        link:"https://www.yoox.com/images/items/17/17427383NQ_14_f.jpg?impolicy=crop&width=306&height=390"
+    },
+    {
+        id:9,
+        title:"RALPH",
+        category:"Casual pants",
+        offer:"40% OFF",
+        price:"$69.00",
+        link:"https://www.yoox.com/images/items/13/13607468BP_14_f.jpg?impolicy=crop&width=306&height=390"
+    },
+
+]
