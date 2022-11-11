930bdcaf4c2cb33c5823b0a431e16ce4259c826e
LV-23 <lokeshvyavhare23@gmail.com>
2022-11-11 21:08:08 +0530

    Added Admin Page

--- Front-end/src/Components/Admin/Admin.AddProduct.jsx
+++ Front-end/src/Components/Admin/Admin.AddProduct.jsx
@@ -11,16 +11,25 @@ export const AddProduct = () => {
                 <input type="text" placeholder='Enter Name of the product'/>
                 <input type="text" placeholder="Enter Description"/>
                 <Flex justify='space-between'>
-                    <Select placeholder='Select option' w='45%'>
-                        <option value='option1'>Option 1</option>
-                        <option value='option2'>Option 2</option>
-                        <option value='option3'>Option 3</option>
+                    <Select placeholder='Select Category' w='45%'>
+                        <option value='option1'>Men</option>
+                        <option value='option2'>Women</option>
+                        <option value='option3'>Kids</option>
                     </Select>
-                    <Select placeholder='Select option' w='45%'>
-                        <option value='option1'>Option 1</option>
-                        <option value='option2'>Option 2</option>
-                        <option value='option3'>Option 3</option>
+                    <Select placeholder='Select SubCategory' w='45%'>
+                        <option value='option1'>Footwear</option>
+                        <option value='option2'>Upperwear</option>
+                        <option value='option3'>Lowerwear</option>
+                        <option value='option3'>Innerwear</option>
+                        <option value='option3'>Jwellery</option>
+                        <option value='option3'>Hats</option>
+                        <option value='option3'>Tradition</option>
+                        <option value='option3'>Fullbody</option>
                     </Select>
+                </Flex>
+                <Flex className={style.sectionAA1}>
+                <label><input type="number" placeholder="Enter price"/> $</label>
+                <label><input type="number" placeholder="Enter Discount"/> %</label>
                 </Flex>
             </Box>
             <Box className={style.sectionA2}>
@@ -36,7 +45,7 @@ export const AddProduct = () => {
                 </Box>
             </Box>
         </Flex>
-        <button type="submit"></button>
+        <button type="submit">Add Product</button>
         </Box>
 
     </Box>
