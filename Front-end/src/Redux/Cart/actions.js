c9c68a8fe881a9afbdb87c68fac692df4b3b626a
LV-23 <lokeshvyavhare23@gmail.com>
2022-11-11 17:42:17 +0530

    Added Auth redux

---
+++ Front-end/src/Redux/Cart/actions.js
@@ -0,0 +1,33 @@
+import {
+    Cart_Add_Items_Error,
+    Cart_Add_Items_Loading,
+    Cart_Add_Items_Success,
+    Cart_Delete_Items_Error,
+    Cart_Delete_Items_Loading,
+    Cart_Delete_Items_Success,
+    Cart_Get_Items_Error,
+    Cart_Get_Items_Loading,
+    Cart_Get_Items_Success,
+    Cart_Update_Items_Error,
+    Cart_Update_Items_Loading,
+    Cart_Update_Items_Success
+} from './actionTypes'
+import {useSelector} from 'react-redux';
+
+
+export const AddToCart = (id) => async (dispatch) => {
+    const user = useSelector(store=>store.auth.data.token.split('-')[0]);
+
+    dispatch({type:Cart_Add_Items_Loading})
+    if(!token){
+
+    }else{
+        try{
+            let req =await axios.post(`https://wardrobe-server.onrender.com/carts/`, {token})
+
+        }catch(err){
+            dispatch({type:Cart_Add_Items_Loading, payload:err})
+        }
+    }
+
+}
