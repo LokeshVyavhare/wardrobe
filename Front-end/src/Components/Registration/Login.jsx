e730ebc0adb2bd5dc86fd58a598686cee88a19a3
Shivang kumar <103144321+kshivang80@users.noreply.github.com>
2022-11-11 21:20:45 +0530

    Login link added

--- Front-end/src/Components/Registration/Login.jsx
+++ Front-end/src/Components/Registration/Login.jsx
@@ -1,15 +1,13 @@
 import { Button } from "@chakra-ui/react"
 import { BiLock } from 'react-icons/bi'
 export const Login = () => {
-    return <div>
+    return (
+        <div>
+            <div style={{ marginTop: "200px", height: "500px", border: "1px solid red" }}>
 
-        <div style={{display:"flex"}}>
-            <BiLock  style={{height:"50px",width:"21%",marginTop:"-10"}}/>
-            <Button colorScheme='gray' variant='ghost' color="black" size='sm' >
-                Login
-            </Button>
-
+                <h1>This is Login page</h1>
+            </div>
         </div>
 
-    </div>
+    )
 }
