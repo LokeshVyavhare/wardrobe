8a01e34f27ba05f6896d18bc8b59286a9987b3fe
LV-23 <lokeshvyavhare23@gmail.com>
2022-11-12 11:42:59 +0530

    A

--- Front-end/src/Components/Cart/CartItem.jsx
+++ Front-end/src/Components/Cart/CartItem.jsx
@@ -7,12 +7,18 @@ import {
 
 } from '@chakra-ui/react'
 import style from './cart.module.css'
-import {useDispatch} from 'react-redux'
+import {useDispatch, useSelector} from 'react-redux'
+import { updateCartItem } from '../../Redux/Cart/actions'
 
 export const CartItem = ({data}) => {
-    const {quantity, product} = data
+    const {quantity, product, cartId} = data
     const {id, name, image1, category, price, tags} = product;
     const dispatch = useDispatch()
+    const token = useSelector(store=>store.auth.data.token);
+
+    const addCount = ()=>{
+        dispatch(updateCartItem(token, cartId, quantity+1))
+    }
 
 
     return <Flex justify={'space-around'} align={'center'}  direction={['column', 'column', 'row']} mb={['35px', '35px', '25px']}>
